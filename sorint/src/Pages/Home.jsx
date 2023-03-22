6865a27f596578fba8729487df7fbb262409544f
Laila <124719945+lailaz5@users.noreply.github.com>
2023-03-22 15:59:14 +0100

    Modifica del testo nella Home
    Co-Authored-By: ElPopov03 <120571791+ElPopov03@users.noreply.github.com>

--- sorint/src/Pages/Home.jsx
+++ sorint/src/Pages/Home.jsx
@@ -8,14 +8,25 @@ const Home = () =>{
             </video>
           </div>
           <p className="gTitolo">"COME A CASA <span style={{color: '#a50000'}}>MA PIÙ IN GRANDE</span>" - Star</p>
-          <p class ="sottotesto">Tracker di Punteggi per Ping Pong:<br/>Progetto PCTO che sfrutta OpenCV, libreria di python per tracciare il percorso di oggetti, nel nostro caso,della pallina.
-          Se questa lascia il campo per più di 1s (quindi fuori dalla portata della webcam) il punto va alla persona che ha tirato, seguendo le regole. Bisogna ancora intraprendere la strada per 
-          il tracciamento e l'eventuale evento che si va a creare appena la pallina esce dal campo.</p>
+          <p class ="sottotesto">Tracker di Punteggi per Ping Pong:<br/>Il presente progetto di PCTO si avvale dell'utilizzo della libreria OpenCV di Python 
+          al fine di tracciare il percorso degli oggetti in gioco, nello specifico, la pallina. In caso la suddetta dovesse abbandonare il campo di gioco per 
+          un intervallo di tempo maggiore di un secondo, fuori dal campo visivo della webcam, il punto verrà assegnato alla persona che ha effettuato il tiro, 
+          attenendosi alle regole stabilite. Tuttavia, si rende necessario ancora l'avvio dell'implementazione del processo di tracciamento e la definizione 
+          dell'evento che avverrà in concomitanza con l'uscita della pallina dal campo di gioco.</p>
           {/* <div className="txtimg">
             <img className="imgDestra" src="https://imgix.bustle.com/inverse/43/e9/9e/ac/0c64/4d0e/a2bc/dda1d61a31db/on-fire.jpeg?w=350&h=298&fit=crop&crop=faces&auto=format%2Ccompress&blend=000000&blendAlpha=45&blendMode=normal" alt="" />
           </div> */}
           <p className="gTitolo">"<span style={{color: '#a50000'}}>DREAM</span> TEAM" - I.G.N.</p>
-          <p class ="sottotesto">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          <p class ="sottotesto">Il tracking di una pallina da ping pong è un tema di grande attualità e interesse, grazie all'ampia gamma di applicazioni che questo processo può offrire. 
+          In particolare, in ambito sportivo, il tracciamento della pallina consente di ottenere informazioni dettagliate sulla prestazione dei giocatori e 
+          sull'andamento della partita, rendendolo un'utile strumento per l'allenamento e la formazione tecnica degli atleti. Grazie all'analisi dei dati raccolti durante il gioco, 
+          è possibile migliorare la velocità di reazione e la precisione dei tiri, individuando eventuali errori e correggendoli per ottenere una maggiore efficacia in campo.
+          Inoltre, il tracciamento della pallina può essere utilizzato anche a scopo didattico, in particolare nell'ambito scolastico, per insegnare agli studenti le regole del 
+          gioco del ping pong e per introdurli alle tecniche di gioco. Grazie all'uso di strumenti tecnologici, come i software di tracciamento, gli studenti possono apprendere 
+          in modo più coinvolgente e interattivo, rendendo più efficace il processo di insegnamento e di apprendimento.
+          Infine, il tracciamento della pallina può essere adottato anche in ambito industriale, per la realizzazione di sistemi di automazione che utilizzano il rilevamento 
+          ottico degli oggetti per controllare e gestire i processi di produzione. Ad esempio, nei processi di confezionamento di prodotti alimentari, il tracciamento della 
+          pallina può essere utilizzato per verificare la corretta posizione dei prodotti all'interno dell'imballaggio, garantendo così la qualità e la sicurezza dei prodotti stessi.</p>
 
     </>
     )
