3c9342f420e4e09315e1e394f3d7b8bc5afcc0e3
TgCas24 <casalixluca@gmail.com>
2023-05-29 18:41:38 +0200

    Correzione
    Co-Authored-By: Giavuz <93006910+Giavuz@users.noreply.github.com>

--- sorint/src/Components/Riquadri.jsx
+++ sorint/src/Components/Riquadri.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { RiquadriPersone } from "../Js/riquadri";
 
 const Riquadri = () => {
+
   return (
     <>
       <div className="divPrinc">
         {RiquadriPersone.map((item, index) => {
           return (
             <div className="divRiquadri" key={index}>
-              <img className="imgRiquadri" src={`/${item.img}`} alt={item.alt} onError={(e) => {e.target.style.display = 'none'}} />
+              {/* <img className="imgRiquadri" src={`/${item.img}`} alt={item.alt} onError={(e) => {e.target.style.display = 'none'}} /> */}
               <div className="immaginiAboutUs" style={{ backgroundImage: `url(${item.img})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
                 <br />
                 <br />
