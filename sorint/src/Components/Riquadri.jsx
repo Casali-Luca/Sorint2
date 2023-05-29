5681be4b02ab15e80769246f69fd46c5a6166074
Laila <124719945+lailaz5@users.noreply.github.com>
2023-05-29 17:50:48 +0200

    Sistemazione finale pagina about us
    Aggiornato il component Riquadri per gestire correttamente la visualizzazione delle immagini e del testo alternativo.

--- sorint/src/Components/Riquadri.jsx
+++ sorint/src/Components/Riquadri.jsx
@@ -1,33 +1,26 @@
 import React from "react";
 import { RiquadriPersone } from "../Js/riquadri";
+
 const Riquadri = () => {
-
-    return(
-        <>
-            <div className="divPrinc">
-                {RiquadriPersone.map((item)=>{
-                    let background = item.img;
-                    console.log(background);
-                    return(
-                    <>
-                    <div className="divRiquadri"> 
-                        <img className="imgRiquadri" src={`/${item.img}`} />
-                        <div className= "immaginiAboutUs" style={{backgroundImage: `url(${background})`,
-                            backgroundPosition: 'center',
-                            backgroundRepeat: 'no-repeat' 
-                        }}>
-                            <br />
-                            <br />
-                        </div>
-                        <h1 className="titoloRiquadri">{item.title}</h1>
-                        <p className="pRiquadri">{item.p}</p>
-                    </div>
-                    </>
-                )})}
-
+  return (
+    <>
+      <div className="divPrinc">
+        {RiquadriPersone.map((item, index) => {
+          return (
+            <div className="divRiquadri" key={index}>
+              <img className="imgRiquadri" src={`/${item.img}`} alt={item.alt} onError={(e) => {e.target.style.display = 'none'}} />
+              <div className="immaginiAboutUs" style={{ backgroundImage: `url(${item.img})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+                <br />
+                <br />
+              </div>
+              <h1 className="titoloRiquadri">{item.title}</h1>
+              <p className="pRiquadri">{item.p}</p>
             </div>
-        </>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Riquadri;
